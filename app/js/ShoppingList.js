298bd4d10d94874c05c8be9e04adc0aa28124846
shangwee <hoshangwee0911@gmail.com>
2023-10-09 22:55:41 +0800

    edited shoppinglist able to add and display item

--- app/js/ShoppingList.js
+++ app/js/ShoppingList.js
@@ -9,9 +9,47 @@ const app = Vue.createApp({
         recipe: "./RecipeList.html",
         profile: "./Profile.html",
       },
+
+      // shopping list
+      sList: [],
     };
   },
-  methods: {},
+  methods: {
+    addItem() {
+      // add item to shopping list
+      // if both inputs are filled
+      if (document.getElementById("foodName").value != "" && document.getElementById("foodQuanity").value != ""){
+        this.sList.push({
+          name: document.getElementById("foodName").value,
+          quantity: document.getElementById("foodQuanity").value,
+        });
+
+        // clear inputs
+        document.getElementById("foodName").value = "";
+        document.getElementById("foodQuanity").value = "";
+        this.displayList();
+      }
+      else
+      {
+        alert("Please fill out both fields");
+      }
+      console.log(this.sList);
+    },
+
+    displayList(){
+      var listing_page = document.getElementById("MainList");
+      // clear everything in div
+      listing_page.innerHTML = "";
+      // add ul 
+      listing_page.innerHTML += "<ul id='list'>";
+      // create lisf of items
+      for (var i = 0; i < this.sList.length; i++){
+        listing_page.innerHTML += "<li>" + this.sList[i].name + " - " + this.sList[i].quantity + "</li>";
+      }
+      // close ul
+      listing_page.innerHTML += "</ul>";
+    }, 
+  },
 });
 
 const vm = app.mount("#shoppingList");
